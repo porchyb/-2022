11c966424f0c46b1c2c851096dbdb9c82c73b767
abdulb228 <kartem025@gmail.com>
2022-11-17 10:51:36 +0600

    Исправил RotateCommand и IRotatable

---
+++ SpaceBattle.Lib.Test/DegreeTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using SpaceBattle;
+using Xunit;
+using Moq;
+
+namespace SpaceBattle.Lib.Test
+{
+    public class DegreeTests
+    {
+        [Fact]
+        public void Equality_DEGandDEG_true()
+        {
+            degree d1 = new(45);
+            degree d2 = new(45);
+            Assert.True(d1 == d2);
+        }
+        [Fact]
+        public void Inequality_DEGandDEG_true()
+        {
+            degree d1 = new(45);
+            degree d2 = new(60);
+            Assert.True(d1 != d2);
+        }
+        [Fact]
+        public void Equals_DEGandDEG_true()
+        {
+            degree d1 = new(45);
+            degree d2 = new(45);
+            Assert.True(d1.Equals(d2));
+        }
+        [Fact]
+        public void Equals_DEGandINT_false()
+        {
+            degree d1 = new(45);
+            int a = 0; ;
+            Assert.False(d1.Equals(a));
+        }
+        [Fact]
+        public void GetHashCode_DEGandDEG_true()
+        {
+            degree d1 = new(45);
+            degree d2 = new(45);
+            Assert.Equal(d1.GetHashCode(), d2.GetHashCode());
+        }
+        [Fact]
+        public void GetHashCode_DEGandDEG_false()
+        {
+            degree d1 = new(45);
+            degree d2 = new(10);
+            Assert.NotEqual(d1.GetHashCode(), d2.GetHashCode());
+        }
+    }
+}
--- SpaceBattle.Lib/degree.cs
+++ SpaceBattle.Lib/degree.cs
@@ -5,42 +5,66 @@ namespace SpaceBattle.Lib
     public class degree
     {
         public double deg;
-      
-        public degree(double angle){
+
+        public degree(double angle)
+        {
             deg = angle;
         }
-        public static degree operator +(degree a, float b){
-            if (Math.Abs(a.deg + b) > 360){                
-                a.deg = (a.deg + b) %360;
+        public static degree operator +(degree a, float b)
+        {
+            if (Math.Abs(a.deg + b) > 360)
+            {
+                a.deg = (a.deg + b) % 360;
                 return a;
             }
             a.deg = a.deg + b;
             return a;
-        }        
-        public static degree operator +(float b, degree a){
-            if (Math.Abs(a.deg + b) > 360){                
-                a.deg = (a.deg + b) %360;
+        }
+        public static degree operator +(float b, degree a)
+        {
+            if (Math.Abs(a.deg + b) > 360)
+            {
+                a.deg = (a.deg + b) % 360;
                 return a;
             }
             a.deg = a.deg + b;
             return a;
         }
 
-        public static degree operator -(degree a, float b){
-            if (Math.Abs(a.deg - b) > 360){                
-                a.deg = -(Math.Abs(a.deg - b) %360);
+        public static degree operator -(degree a, float b)
+        {
+            if (Math.Abs(a.deg - b) > 360)
+            {
+                a.deg = -(Math.Abs(a.deg - b) % 360);
                 return a;
             }
             a.deg = a.deg - b;
             return a;
-        }  
-        public static degree operator -(float b, degree a){            
-            if (Math.Abs(a.deg - b) > 360){                
-                a.deg = -(Math.Abs(a.deg - b) %360);
+        }
+        public static degree operator -(float b, degree a)
+        {
+            if (Math.Abs(a.deg - b) > 360)
+            {
+                a.deg = -(Math.Abs(a.deg - b) % 360);
                 return a;
             }
             a.deg = a.deg - b;
             return a;
-        }    
+        }
+        public static bool operator ==(degree a, degree b)
+        {
+            if (a.deg != b.deg) { return false; }
+            return true;
+        }
+        public static bool operator !=(degree a, degree b)
+        {
+            if (a.deg != b.deg) { return true; }
+            return false;
+        }
+        public override bool Equals(object? obj) => obj is degree v && deg==v.deg;
+        public override int GetHashCode()
+        {
+            return String.Join("", deg.ToString()).GetHashCode();
+        }
     }
 }
