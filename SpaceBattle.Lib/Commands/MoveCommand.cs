bd2f54a41ca4e858d903b50c1bc3c1b1b08e1323
abdulb228 <kartem025@gmail.com>
2022-12-14 16:55:14 +0600

    Revert "Удалил файлы движения"
    This reverts commit 229dcd979af4c3b4495ab0219f1c9f31c9f084bd.

---
+++ SpaceBattle.Lib/Commands/MoveCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceBattle.Lib
+{
+    public class MoveCommand: ICommand
+    {
+        public IMovable movObj;
+
+        public MoveCommand(IMovable obj)
+        {
+            movObj = obj;
+        }
+        public void Execute()
+        {
+            try{
+                movObj.position = movObj.position + movObj.velocity;
+            }
+            catch
+            {
+                throw new ArgumentException();
+            }
+            
+        }
+    }
+}
