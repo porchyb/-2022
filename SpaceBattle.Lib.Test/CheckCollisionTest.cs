c43206d9cb627958c429c6e774341ec37a854317
Dokanin-ssha <sashadokanin@mail.ru>
2023-01-18 21:21:30 +0600

    +1 test

--- SpaceBattle.Lib.Test/CheckCollisionTest.cs
+++ SpaceBattle.Lib.Test/CheckCollisionTest.cs
@@ -13,4 +13,15 @@ public class CheckCollisionTests{
         CheckCollisionCommand command = new CheckCollisionCommand(mockShip1.Object,mockShip2.Object);
         Assert.Throws<Exception>(() => command.Execute());
     }
+
+    [Fact]
+    public void Resolve_String_Execute_2(){
+        Mock<IMovable> mockShip1 = new();        
+        Mock<IMovable> mockShip2 = new();
+        mockShip1.Setup(a=>a.position).Returns(new Vector(15,88));
+        mockShip2.Setup(a=>a.position).Returns(new Vector(9,54));
+        new InitTreeCommand().Execute();
+        CheckCollisionCommand command = new CheckCollisionCommand(mockShip1.Object,mockShip2.Object);
+        
+    }
 }
