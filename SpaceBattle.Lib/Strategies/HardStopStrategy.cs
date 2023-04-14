0e0285e224c85c30c909b11a625ab891c6f6cad6
abdulb228 <kartem025@gmail.com>
2023-04-15 00:27:52 +0600

    More tests

--- SpaceBattle.Lib/Strategies/HardStopStrategy.cs
+++ SpaceBattle.Lib/Strategies/HardStopStrategy.cs
@@ -4,7 +4,7 @@ using Accord.MachineLearning.DecisionTrees;
 public class HardStopStrategy: IStrategy{
     public object UseStrategy(params object[] args){
         int id = (int)args[0];
-        Action action = args.Length > 1 ? (Action)args[1] : null;
+        Action? action = args.Length > 1 ? (Action)args[1] : null;
         return !(action is null)
             ? new ThreadHardStopCommand(id, action)
             : new ThreadHardStopCommand(id);
