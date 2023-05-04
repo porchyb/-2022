02269ccc52531ccdae943d0635dbad7624d950c2
abdulb228 <kartem025@gmail.com>
2023-05-04 12:02:48 +0600

    Edit UpdateBehaviour test

--- SpaceBattle.Lib.Test/ServerTests.cs
+++ SpaceBattle.Lib.Test/ServerTests.cs
@@ -62,6 +62,7 @@ public class ServerTests{
         MyThread thread = IoC.Resolve<ConcurrentDictionary<int, MyThread>>("Game.ThreadDictionary")[1];
         thread.UpdateBehaviour(action);
         Thread.Sleep(500);
+        IoC.Resolve<ICommand>("Game.HardStopThreadCommand", 1).Execute();
         mockCommand.Verify(a=>a.Execute());
     }
 
