27c30b675a9fba08d7c6542aa901d61d4aaf5039
abdulb228 <kartem025@gmail.com>
2023-05-24 20:38:51 +0600

    Endpoint test

--- SpaceBattle.Lib.Test/EndpointTests.cs
+++ SpaceBattle.Lib.Test/EndpointTests.cs
@@ -1,26 +1,47 @@
-/*using System;
+using System;
 using SpaceBattle;
 using Xunit;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.AspNetCore.Hosting;
 using Moq;
-
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net.Http;
+using System.Net.Http.Json;
 
 namespace SpaceBattle.Lib.Test
 {
     public class EndpointTest
     {
-        private HttpClient _client;
+        private readonly HttpClient client = new() {BaseAddress = new Uri("http://localhost:5001") };
         public EndpointTest()
         {
-            Endpoint.Run();
-            var server = new TestServer(new WebHostBuilder)
+            Mock<ICommand> mockSendCommand = new();
+            mockSendCommand.Setup(a => a.Execute());
+            Mock<IStrategy> sendStrategy = new();
+            sendStrategy.Setup(a => a.UseStrategy(It.IsAny<object[]>())).Returns(mockSendCommand.Object);
+            IoC.Resolve<ICommand>("IoC.Add", "Game.SendCommand", sendStrategy.Object).Execute();
+
+            Mock<ICommand> mockTestCommand = new();
+            Mock<IStrategy> commandStrategy = new();
+            commandStrategy.Setup(a => a.UseStrategy(It.IsAny<object[]>())).Returns(mockTestCommand.Object);
+            IoC.Resolve<ICommand>("IoC.Add", "Game.TestCommand", commandStrategy.Object).Execute();
+            //Endpoint.Stop();
         }
-        [Theory]
-        [InlineData("GET")]
-        public async Task SendMessage(string method)
+
+        [Fact]
+        public async Task Endpoint_PostMessage_OK()
         {
-            
+            /*var thread = new Thread(() => { Endpoint.Run(); });
+            thread.IsBackground = true;
+            thread.Start();*/
+            //await Endpoint.Run();s
+            //throw new Exception();
+            var expectedStatusCode = System.Net.HttpStatusCode.OK;
+            var message = new Message("TestCommand", 1, new[] { "par1", "par2" });
+            JsonContent content = JsonContent.Create(message);
+            var response = await client.PostAsync("/message", content);
+
+            Assert.Equal(expectedStatusCode, response.StatusCode);
         }
     }
-}*/
+}
--- SpaceBattle.Lib/Endpoint/Endpoint.cs
+++ SpaceBattle.Lib/Endpoint/Endpoint.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
+using Microsoft.AspNetCore.Mvc;
 
 namespace SpaceBattle.Lib
 {
@@ -11,51 +12,38 @@ namespace SpaceBattle.Lib
         static WebApplication? app;
         public static void Run()
         {
+            /*var builder = WebApplication.CreateBuilder();
+            var app = builder.Build();
+            app.UseHttpsRedirection();*/
+
             WebApplicationBuilder webApplicationBuilder = WebApplication.CreateBuilder();
             app = webApplicationBuilder.Build();
-            app.Map("/", async (context) =>
+            app.UseHttpsRedirection();
+            app.MapPost("/message", (Message message) =>
             {
-                var response = context.Response;
-                await response.WriteAsync("Void");
+                //var response = context.Response;
+                //var request = context.Request;
+                //Message? message;
+                //message = await request.ReadFromJsonAsync<Message>();
+                try
+                {
+                    ICommand cmd = IoC.Resolve<ICommand>("Game." + message.cmd, message.cmdParams);
+                    IoC.Resolve<ICommand>("Game.SendCommand", message.gameId, cmd).Execute();
+                }
+                catch
+                {
+                    throw new Exception("command deserealization error");
+                }
+                return Results.Ok(message);
             });
-            app.MapPost("/message", async (context) =>
+            app.MapGet("/", () =>
             {
-                var response = context.Response;
-                var request = context.Request;
-                Message? message;
-                message = await request.ReadFromJsonAsync<Message>();
-                if (!(message is null))
-                {
-                    try
-                    {
-                        int gameId = message.gameId;
-                        object[] cmdParams = message.cmdParams;
-                        try
-                        {
-                            ICommand cmd = IoC.Resolve<ICommand>("Game." + message.cmd, cmdParams);
-                            IoC.Resolve<ICommand>("Game.SendCommand", gameId, cmd).Execute();
-                        }
-                        catch
-                        {
-                            throw new Exception("command deserealization error");
-                        }
-                    }
-                    catch
-                    {
-                        throw new Exception("Incorrect view of JSON");
-                    }
-                    
-
-
-                }
-                else
-                {
-                    Console.WriteLine("wrong");
-                    await response.WriteAsync("wrong");
-                }
+                //var response = context.Response;
+                //await response.WriteAsync("Void");
+                return Results.Ok();
             });
             app.Run();
         }
-        public record Message(string cmd, int gameId, object[] cmdParams);
     }
+    public record Message(string cmd, int gameId, object[] cmdParams);
 }
