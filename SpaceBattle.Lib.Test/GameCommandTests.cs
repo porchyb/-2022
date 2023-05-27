0a6525b6391a8fab122745f4196f63e3a1dc3e6f
abdulb228 <kartem025@gmail.com>
2023-05-27 21:17:59 +0600

    GameCommand exception test

--- SpaceBattle.Lib.Test/GameCommandTests.cs
+++ SpaceBattle.Lib.Test/GameCommandTests.cs
@@ -41,5 +41,32 @@ namespace SpaceBattle.Lib.Test
 
             Assert.True(gameQueue.Count == 0);
         }
+
+        [Fact]
+        public void GameCommand_Id_CommandException()
+        {
+            Mock<ICommand> mockCommand = new();
+            mockCommand.Setup(a => a.Execute()).Throws(new Exception());
+            Queue<ICommand> gameQueue = new() { };
+            gameQueue.Enqueue(mockCommand.Object);
+            Mock<IStrategy> mockQueueStrategy = new();
+            mockQueueStrategy.Setup(a => a.UseStrategy()).Returns(gameQueue);
+
+            Mock<IStrategy> mockTimeQuantumStrategy = new();
+            mockTimeQuantumStrategy.Setup(a => a.UseStrategy()).Returns(500);
+
+            var gameScope = new Dictionary<string, IStrategy>{
+                {"Game.TimeQuantum", mockTimeQuantumStrategy.Object},
+                {"Game.Queue", mockQueueStrategy.Object}
+            };
+            var gameScopes = new Dictionary<int, Dictionary<string, IStrategy>>{
+                {1, gameScope}
+            };
+            Mock<IStrategy> mockGameScopesStrategy = new();
+            mockGameScopesStrategy.Setup(a => a.UseStrategy(It.IsAny<object[]>())).Returns(gameScopes);
+            IoC.Resolve<ICommand>("IoC.Add", "IoC.GameScopes", mockGameScopesStrategy.Object).Execute();
+
+            new GameCommand(1).Execute();
+        }
     }
 }
