a3a7380d2c9ee9283cba030bd58b2f798dad9517
abdulb228 <kartem025@gmail.com>
2023-05-29 21:03:00 +0600

    ExceptionHandler test

--- SpaceBattle.Lib.Test/ExceptionHandlerTests.cs
+++ SpaceBattle.Lib.Test/ExceptionHandlerTests.cs
@@ -17,12 +17,36 @@ namespace SpaceBattle.Lib.Test
         }
 
         [Fact]
-        public void ExceptionHandler_()
+        public void ExceptionHandler_DefaultStrategy()
         {
             Mock<ICommand> mockCommand = new();
             var ex = new Exception();
             var strategy = IoC.Resolve<IStrategy>("Handler.Handle", mockCommand.Object, ex);
             Assert.Throws<Exception>(() => strategy.UseStrategy());
+        }
+        [Fact]
+        public void ExceptionHandler_NotDefaultStrategy()
+        {
+            Mock<ICommand> mockCommand = new();
+            var ex = new Exception();
+
+            Mock<IStrategy> mockStrategy = new();
+            mockStrategy.Setup(a => a.UseStrategy()).Throws(new DivideByZeroException()); //only for test
+
+            Mock<IStrategy> mockGetStrategy = new();
+            mockGetStrategy.Setup(a => a.UseStrategy()).Returns(mockStrategy.Object);
+
+            var handlerStrategies = new Dictionary<Type, Dictionary<Exception, IStrategy>>()
+            {
+                {
+                    mockCommand.Object.GetType(), new Dictionary<Exception, IStrategy>{ {ex, mockStrategy.Object } } 
+                }
+            };
+            IoC.Resolve<ICommand>("IoC.Add", "Handler.Strategies", new GetExceptionStrategies(handlerStrategies)).Execute();
+
+            
+            var strategy = IoC.Resolve<IStrategy>("Handler.Handle", mockCommand.Object, ex);
+            Assert.Throws<DivideByZeroException>(() => strategy.UseStrategy());
         }
     }
 }
