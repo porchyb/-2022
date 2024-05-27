fbdad29fe90e632b34aa9ecd1bea572e20d891c7
Dokanin-ssh <sashadokanin@mail.ru>
2024-05-28 01:34:09 +0600

    Added FStrategy

---
+++ SpaceBattle.Lib/Strategies/GetGameUObjectStrategy.cs
@@ -0,0 +1,16 @@
+namespace SpaceBattle.Lib;
+
+public class GetGameUObjectStrategy : IStrategy { 
+    public object UseStrategy(params object[] args)
+    {
+        int objectid = (int)args[0];
+
+        IUObject? uObject;
+
+        if (IoC.Resolve<IDictionary<int, IUObject>>("Game.UObject.Map").TryGetValue(objectid, out uObject))
+        {
+            return uObject;
+        }
+        throw new Exception();
+    }
+}
