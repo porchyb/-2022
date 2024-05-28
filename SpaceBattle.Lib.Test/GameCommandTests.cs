f7da39adbc4882cb8cdf317b781c6c9fc8650a1c
Dokanin-ssh <sashadokanin@mail.ru>
2024-05-29 04:13:51 +0600

    fixs

---
+++ SpaceBattle.Lib.Test/GameCommandTests.cs
@@ -0,0 +1,76 @@
+﻿using Moq;
+using SpaceBattle.Lib.Commands;
+using System;
+using Xunit;
+
+namespace SpaceBattle.Lib.Test
+{
+    public class GameCommandTests
+    {
+        public GameCommandTests()
+        {
+            var handlerStrategies = new Dictionary<Type, Dictionary<Exception, IStrategy>>();
+            IoC.Resolve<ICommand>("IoC.Add", "Handler.Handle", new ExceptionHandlerStrategy()).Execute();
+            IoC.Resolve<ICommand>("IoC.Add", "Handler.Strategies", new GetExceptionStrategies(handlerStrategies)).Execute();
+            IoC.Resolve<ICommand>("IoC.Add", "Handler.Default", new GetExceptionHandlerDefaultStrategy()).Execute();
+            IoC.Resolve<ICommand>("IoC.Add", "IoC.SetScopeCommand", new SetScopeStrategy()).Execute();
+            IoC.Resolve<ICommand>("IoC.Add", "IoC.DeleteScopeCommand", new DeleteScopeStrategy()).Execute();
+        }
+
+        [Fact]
+        public void GameCommand_Id_Success()
+        {
+            Mock<ICommand> mockCommand = new();
+            mockCommand.Setup(a => a.Execute()).Verifiable();
+            Queue<ICommand> gameQueue = new() { };
+            gameQueue.Enqueue(mockCommand.Object);
+            Mock<IStrategy> mockQueueStrategy = new();
+            mockQueueStrategy.Setup(a => a.UseStrategy()).Returns(gameQueue);
+
+            Mock<IStrategy> mockTimeQuantumStrategy = new();
+            mockTimeQuantumStrategy.Setup(a => a.UseStrategy()).Returns(500);
+
+            var gameScope = new Dictionary<string, IStrategy>{
+                {"Game.TimeQuantum", mockTimeQuantumStrategy.Object},
+                {"Game.Queue", mockQueueStrategy.Object}
+            };
+            var gameScopes = new Dictionary<int, Dictionary<string, IStrategy>>{
+                {1, gameScope}
+            };
+            Mock<IStrategy> mockGameScopesStrategy = new();
+            mockGameScopesStrategy.Setup(a => a.UseStrategy(It.IsAny<object[]>())).Returns(gameScopes);
+            IoC.Resolve<ICommand>("IoC.Add", "IoC.GameScopes", mockGameScopesStrategy.Object).Execute();
+
+            new GameCommand(1).Execute();
+
+            Assert.True(gameQueue.Count == 0);
+        }
+
+        [Fact]
+        public void GameCommand_Id_CommandException()
+        {
+            Mock<ICommand> mockCommand = new();
+            mockCommand.Setup(a => a.Execute()).Throws(new Exception());
+            Queue<ICommand> gameQueue = new() { };
+            gameQueue.Enqueue(mockCommand.Object);
+            Mock<IStrategy> mockQueueStrategy = new();
+            mockQueueStrategy.Setup(a => a.UseStrategy()).Returns(gameQueue);
+
+            Mock<IStrategy> mockTimeQuantumStrategy = new();
+            mockTimeQuantumStrategy.Setup(a => a.UseStrategy()).Returns(500);
+
+            var gameScope = new Dictionary<string, IStrategy>{
+                {"Game.TimeQuantum", mockTimeQuantumStrategy.Object},
+                {"Game.Queue", mockQueueStrategy.Object}
+            };
+            var gameScopes = new Dictionary<int, Dictionary<string, IStrategy>>{
+                {1, gameScope}
+            };
+            Mock<IStrategy> mockGameScopesStrategy = new();
+            mockGameScopesStrategy.Setup(a => a.UseStrategy(It.IsAny<object[]>())).Returns(gameScopes);
+            IoC.Resolve<ICommand>("IoC.Add", "IoC.GameScopes", mockGameScopesStrategy.Object).Execute();
+
+            Assert.Throws<Exception>(() => new GameCommand(1).Execute());
+        }
+    }
+}
